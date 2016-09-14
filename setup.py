86ca60bc2f20e7220c6757a222050f6616862774
quincymd <quincymd@mail.com>
2016-09-14 16:49:53 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#---------------------------------------------
+# Script:   
+# Name:     Javier
+# Date:        Wed Sep 14 15:40:56 BST 2016
+# Description:  
+#---------------------------------------------
+
+from setuptools import setup
+
+setup(name="exifgps",
+      version='1.0',
+      description="EXIF GPS information to Google Maps url",
+      url="http://github.com/quincymd/imagegps",
+      author="Cambell Spong",
+      author_email="quincymd@mail.com",
+      license="CC",
+      packages=[],
+      zip_safe=False)
+
+
